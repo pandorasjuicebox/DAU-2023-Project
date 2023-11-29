09e067c8b27f0332f260de09431fa219f664d473
charina <charinaduenas@gmail.com>
2023-11-29 03:02:46 -0600

    added LevelManager.h

--- GameTest/GameObject.h
+++ GameTest/GameObject.h
@@ -4,22 +4,27 @@
 
 class GameObject {
 public: 
-	
+	void SetMoveable(); //false by default
+	bool IsMoveable(); //return isMoveable value
+	//void SetObjHealth(int healthTotal);
+	//void ReduceObjHealth(int lostHealth);
 
 
 
 private:
-	HitBox objectHitBox; //each object has its own HitBox
-	//int health; note: still thinking about this variable
+	HitBox objHitBox; //each object has its own HitBox
+	//int objHealth; note: still thinking about this variable
 	bool isObstacle = false; //default
 	//TODO: make a HitBox object
 	bool isActiveObject = true; //is it interactable or not
 	bool isVisibleObject = true; //is it on the map right now
+	bool isMoveable = false; //default
 	const char* spriteFileName; 
 
 	//default is that it's only one square/one tile unit
 	unsigned int nColumns = 0; 
 	unsigned int nRows = 0;
+
 
 
 };
---
+++ GameTest/LevelManager.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class LevelManager {
+public: 
+
+private:
+};
